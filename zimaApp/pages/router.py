57487ba3197ad26ecc226b2888c00d09fcd25700
Ilmir112 <137054729+Ilmir112@users.noreply.github.com>
2025-06-19 16:59:38 +0500

    html добавление plan

--- zimaApp/pages/router.py
+++ zimaApp/pages/router.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 
-
 from zimaApp.norms.router import find_norms_one
 from zimaApp.pages.dao import ChangeExcelToHtml
-from zimaApp.users.dependencies import get_current_user
+from zimaApp.users.dependencies import get_current_user, get_current_admin_user
 from zimaApp.users.models import Users
+from zimaApp.users.router import login_user
 from zimaApp.wells_repair_data.router import find_well_filter_by_number, find_wells_in_repairs, find_work_plan_all
 
 router = APIRouter(
@@ -14,6 +14,17 @@ router = APIRouter(
 )
 
 templates = Jinja2Templates(directory="zimaApp/templates")
+
+
+@router.get("/home")
+async def get_home_page(
+        request: Request,
+        user: Users = Depends(get_current_user)
+):
+    return templates.TemplateResponse(
+        "home.html", context={
+            "request": request
+        })
 
 
 @router.get("/plan_work")
@@ -26,10 +37,11 @@ async def get_plan_page(
 
     return templates.TemplateResponse(
         "work_plan.html", context={
-        "request": request,
-        "wells_repair": excel_answer
+            "request": request,
+            "wells_repair": excel_answer
 
         })
+
 
 @router.get("/norms")
 async def get_norms_page(
@@ -41,7 +53,7 @@ async def get_norms_page(
 
     return templates.TemplateResponse(
         name="work_plan.html", context={
-        "request": request,
-        "wells_repair": excel_answer
+            "request": request,
+            "wells_repair": excel_answer
 
         })
