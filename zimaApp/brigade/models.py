8ce231c4d654d261153c1fac7baf5f431e63aa72
Ilmir112 <137054729+Ilmir112@users.noreply.github.com>
2025-07-16 15:38:34 +0500

    Исправил порт 80

--- zimaApp/brigade/models.py
+++ zimaApp/brigade/models.py
@@ -21,30 +21,3 @@ class Brigade(Base):
     brigade_composition = Column(JSON)
     pvo_type = Column(String, nullable=False)
     number_pvo = Column(Integer, nullable=False)
-#
-#
-# class Brigade(Base):
-#     __tablename__ = "brigade"
-#
-#     id: int = Column(Integer, primary_key=True)
-#     number: int = Column(Integer, ForeignKey("wells_data.id"))
-#     category_dict: dict = Column(JSON, nullable=False)
-#     type_kr: str = Column(String, nullable=False)
-#     work_plan: str = Column(String, nullable=False)
-#     excel_json: dict = Column(JSON, nullable=False)
-#     data_change_paragraph: dict = Column(JSON, nullable=False)
-#     norms_time: float = Column(Float, nullable=False)
-#     chemistry_need: dict = Column(JSON, nullable=False)
-#     geolog_id = Column(Integer, ForeignKey('users.id'))
-#     date_create: Date = Column(Date, nullable=False)
-#     perforation_project: dict = Column(JSON)
-#     type_absorbent: str = Column(String)
-#     static_level: float = Column(Float, nullable=True)
-#     dinamic_level: float = Column(Float, nullable=True)
-#     expected_data: dict = Column(JSON, nullable=False)
-#     curator: str = Column(String, nullable=False)
-#     region: str = Column(String, nullable=False)
-#     contractor: str = Column(String, nullable=False)
-#
-#     well_data = relationship("WellsData", back_populates="repairs")
-#     users = relationship("Users", back_populates="wells_repairs")
