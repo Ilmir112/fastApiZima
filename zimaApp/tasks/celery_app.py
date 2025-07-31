8b0a48fab64ecd5fd666496063bb83b3de50683f
ilmir112 <zufaroff_112@gmail.com>
2025-07-31 11:38:40 +0500

    обновление repair_gis.js + ручка

--- zimaApp/tasks/celery_app.py
+++ zimaApp/tasks/celery_app.py
@@ -2,18 +2,18 @@ from celery import Celery
 
 from zimaApp.config import settings
 
-# celery_app = Celery(
-#     "tasks",
-#     broker=settings.rabbitmq_url,
-#     backend="rpc://",
-#     include=["zimaApp.tasks.tasks"],
-# )
-
 celery_app = Celery(
     "tasks",
-    broker=f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
+    broker=settings.rabbitmq_url,
+    backend="rpc://",
     include=["zimaApp.tasks.tasks"],
 )
+
+# celery_app = Celery(
+#     "tasks",
+#     broker=f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
+#     include=["zimaApp.tasks.tasks"],
+# )
 
 celery_app.conf.beat_schedule = {
     'check-emails-every-2-minutes': {
